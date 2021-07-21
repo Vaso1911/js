f0517ca00acdb108198d4c43aedd24a8f6616f9a
Alexandr <79134038382@mail.ru>
2021-07-22 02:51:29 +0700

    js

--- js/main.js
+++ js/main.js
@@ -15,23 +15,18 @@ new Swiper('.swiper-container', {
 		}
 	}
 });
+*/
 
-const menuButton = document.querySelector('.menu-button');
-const menu = document.querySelector('.header');
-menuButton.addEventListener('click', function () {
-	menuButton.classList.toggle('menu-button-active');
-	menu.classList.toggle('header-active');
-})
- */
+
 
 const getElemet = (tagName, classNames, attributes) => {
 	const element = document.createElement(tagName);
 
-	if(classNames) {
+	if (classNames) {
 		element.classList.add(...classNames)
 	}
 
-    if (attributes) {
+	if (attributes) {
 		for (const attribute in attributes) {
 			console.log(attribute);
 			element[attribute] = attributes[attribute];
@@ -41,117 +36,260 @@ const getElemet = (tagName, classNames, attributes) => {
 	return element;
 };
 
-const createHeader = (param) => {
-    const header = getElemet('header',);
+// ДЗ-2
+const createHeader = ({ title, header: { logo, menu, social, burger } }) => {
+	const header = getElemet('header',);
 	const container = getElemet('div', ['container']);
+	header.append(container);
 	const wrapper = getElemet('div', ['header']);
+	container.append(wrapper);
 
-    if (param.header.logo) {
+	if (logo) {
 		const logo = getElemet('img', ['logo'], {
-			src: param.header.logo,
-			alt: 'Логотип ' + param.title,
+			src: 'witcher/logo.png',
+			alt: 'Логотип ' + title,
 		});
-	
+
 		wrapper.append(logo);
 	}
 
-    if(param.header.menu) {
-		const menuWrapper = getElemet('nav', ['menu-list']);
-		const menuLinks = param.header.menu.map(items => {
-		const menuLink = getElemet('a', ['menu-link']);
-	
-		menuLink.textContent = items.title;
-		menuLink.href = items.link;
+	if (menu) {
+		const nav = getElemet('nav', ['menu-list']);
+		const allMenuLink = menu.map(item => {
+			const link = getElemet('a', ['menu-link']);
 
-		return menuLink
+			link.textContent = item.title,
+				link.href = item.link
+
+			return link
 		});
-		console.log(menuLinks);
-		menuWrapper.append(...menuLinks);
-		wrapper.append(menuWrapper);
+
+
+
+		nav.append(...allMenuLink);
+		wrapper.append(nav);
 	}
 
 
 
-if(param.header.social) {
-	const socialWrapper = getElemet('div', ['social']);
-	const allSocial = param.header.social.map(item => {
-		const socialLink = getElemet('a', ['social-link']);
-		socialLink.append(getElemet('img', [], {
-			src:item.image,
-			alt:item.title,
-		}));
+	if (social) {
+		const socialWrapper = getElemet('div', ['social']);
+		const allSocial = social.map(item => {
+			const socialLink = getElemet('a', ['social-link']);
+			socialLink.append(getElemet('img', [], {
+				src: item.image,
+				alt: item.title,
+			}));
 
-		socialLink.href = item.link;
+			socialLink.href = item.link;
 
-		return socialLink;
-	});
-	console.log(allSocial);
-	socialWrapper.append(...allSocial);
-	wrapper.append(socialWrapper);
-}
-
-
+			return socialLink;
+		});
+		console.log(allSocial);
+		socialWrapper.append(...allSocial);
+		wrapper.append(socialWrapper);
+	}
 
 
+	if (burger) {
+		const burgerButton = getElemet('button', ['menu-button']);
+		container.append(burgerButton);
+	}
 
-
-
-    header.append(container);
-    container.append(wrapper)
-
+	header.append(container);
+	container.append(wrapper)
 
 	return header;
 };
 
+const createMain = ({ title, main: { genre, rating, description, trailer } }) => {
+
+	const main = getElemet('main');
+	const container = getElemet('div', ['container']);
+	main.append(container);
+	const wrapper = getElemet('div', ['main-content']);
+	container.append(wrapper);
+	const content = getElemet('div', ['content']);
+	wrapper.append(content);
+
+	if (genre) {
+		const genreSpan = getElemet('span', ['genre', 'animated', 'fadeInRight'],
+			{ textContent: genre }
+		);
+
+		content.append(genreSpan)
+	}
+
+	if (rating) {
+		const ratingBlock = getElemet('div', ['raiting', 'animated', 'fadeInRight']);
+		const ratingStars = getElemet('div', ['rating-stars']);
+		const ratingNumber = getElemet('div', ['rating-number'], {
+			textContent: `${rating}/10`
+		});
+
+		for (let i = 0; i < 10; i++) {
+			const star = getElemet('img', ['star'], {
+				alt: i ? '' : `Рейтинг ${rating} из 10`,
+				src: i < rating ? 'img/star.svg' : 'img/star-o.svg'
+			});
+			ratingStars.append(star);
+		}
+
+		ratingBlock.append(ratingStars, ratingNumber);
+		content.append(ratingBlock);
+
+	}
+
+	content.append(getElemet('h1', ['main-title', 'animated', 'fadeInRight'],
+		{ textContent: title }
+	));
+
+	if (description) {
+		content.append(getElemet('p', ['main-description', 'animated', 'fadeInRight'],
+			{ textContent: description },
+		));
+	}
+
+	if (trailer) {
+		const youtubeLink = getElemet('a',
+			['button', 'animated', 'fadeInRight', 'youtube-modal'],
+			{
+				href: trailer,
+				textContent: 'Смотреть трейлер',
+			}
+		);
+
+		const youtubeImgLink = getElemet('a', ['play', 'youtube-modal'],
+			{
+				href: trailer,
+				ariaLabel: 'Смотреть трейлер',
+			}
+		);
+
+		const iconPlay = getElemet('img', ['play-img'],
+			{
+				src: 'img/play.svg',
+				alt: '',
+				ariaHidden: true,
+			}
+		)
+
+
+		content.append(youtubeLink);
+		youtubeImgLink.append(iconPlay);
+		wrapper.append(youtubeImgLink);
+
+	}
+
+
+
+
+	return main;
+};
+
+
+
+
 const movieConstructor = (selector, options) => {
-    
+
 	const app = document.querySelector(selector);
 	app.classList.add('body-app');
 
-    if(options.header) {
-		
+	app.style.backgroundImage = options.background ?
+		`url("${options.background}")` : '';
+
+
+
+	document.link = options.link;
+	document.title = options.title;
+
+
+	if (options.header) {
+
 		app.append(createHeader(options));
+	}
+
+	if (options.main) {
+		app.append(createMain(options))
 	}
 }
 
 
-
 movieConstructor('.app', {
+
+	// не получилось((
+	// link: '/witcher/logo.png',
+	// link: [
+	// 	{
+
+	// 		rel: 'shortcut icon',
+	// 		size: '3x3',
+	// 		type: 'image/png',
+	// 		href: 'witcher/logo.png',
+
+	// 	}
+	// ],
 	title: 'Ведьмак',
+	background: 'witcher/background.jpg',
 	header: {
 		logo: 'witcher/logo.png',
 		social: [
 			{
-				title:'Twitter',
-				link:'https://twitter.com',
-				image:'witcher/social/twitter.svg',
+				title: 'Twitter',
+				link: 'https://twitter.com',
+				image: 'witcher/social/twitter.svg',
 			},
 			{
-				title:'Instagram',
-				link:'https://instagram.com',
-				image:'witcher/social/instagram.svg',
+				title: 'Instagram',
+				link: 'https://instagram.com',
+				image: 'witcher/social/instagram.svg',
 			},
 			{
-				title:'Facebook',
-				link:'https://facebook',
-				image:'witcher/social/facebook.svg',
+				title: 'Facebook',
+				link: 'https://facebook',
+				image: 'witcher/social/facebook.svg',
 			}
 		],
 		menu: [
 			{
-				title:'Описание',
-				link:'#',
+				title: 'Описание',
+				link: '#',
 			},
 			{
-				title:'Трейлер',
-				link:'#',
+				title: 'Трейлер',
+				link: '#',
 			},
 			{
-				title:'Отзывы',
-				link:'#',
+				title: 'Отзывы',
+				link: '#',
 			},
+		],
+		burger: [
+			{
+				button: '',
+			}
 		]
-	}
+
+
+
+
+	},
+	main: {
+		genre: '2019,фэнтези',
+		rating: 8,
+		description: ' Ведьмак Геральт, мутант и убийца чудовищ, на своей верной лошади по кличке Плотва путешествует по Континенту. За тугой мешочек чеканных монет этот мужчина избавит вас от всякой настырной нечисти — хоть от чудищ болотных, оборотней и даже заколдованных принцесс.',
+		trailer: 'https://www.youtube.com/watch?v=P0oJqfLzZzQ',
+	},
 });
+
+// ДЗ-2
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+menuButton.addEventListener('click', function () {
+	menuButton.classList.toggle('menu-button-active');
+	menu.classList.toggle('header-active');
+});
+
+
 
 
